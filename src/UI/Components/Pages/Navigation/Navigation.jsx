c59983b9ed28e8dc95a98115de6c2a931b4a4d34
Crispin <bigizinduwayo@gmail.com>
2022-08-26 12:15:34 +0200

    Updating Navigation

--- src/UI/Components/Pages/Navigation/Navigation.jsx
+++ src/UI/Components/Pages/Navigation/Navigation.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Navigation.css'
 
 function Navigation() {
   return (
-    <div className='container'>
-        <nav>
-            <div className='logo'>
-                <img src="" alt="" />
-            </div>
-            <div className='links'></div>
-        </nav>
-    </div>
+    <nav className='nav'>
+        <div className='logo'>
+            <img src="" alt="" />
+        </div>
+        <div className='links'>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                    <Link to="/mission">Mission</Link>
+                    <Link to="/progress">Progress Report</Link>
+                    <Link to="/map">Rwandan Map</Link>
+                    <Link to="/contact">Contact Us</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
   )
 }
 
