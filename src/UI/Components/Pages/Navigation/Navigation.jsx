3f144c381369a856376555750c7c0ea8ff66bdcf
Crispin <bigizinduwayo@gmail.com>
2022-08-26 11:45:41 +0200

    Project structure

---
+++ src/UI/Components/Pages/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navigation.css'
+
+function Navigation() {
+  return (
+    <div className='container'>
+        <nav>
+            <div className='logo'>
+                <img src="" alt="" />
+            </div>
+            <div className='links'></div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navigation
