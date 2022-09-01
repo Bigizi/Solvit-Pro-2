a929352d65be0f43d778ec3f48c98cff40e758f8
Crispin <bigizinduwayo@gmail.com>
2022-09-01 11:25:02 +0200

    update on form files

--- src/UI/Components/Pages/Login/Login.jsx
+++ src/UI/Components/Pages/Login/Login.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './Login.css'
+import { Link } from 'react-router-dom'
+import Vector from '../../Resources/Images/Vector.png';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <div className='container-3'>
+       <img src={Vector} alt="" id='Back'/>
+        <div className='app-wrapper-2'>
+            
+        <form className='form-wrapper'>
+            <div>
+            <h2 className='title-1'>Login</h2>
+            </div>
+            
+            <div className='name-1'>
+                {/* <label className='label'>Full Name</label> */}
+                <input className='input-field' type='text' placeholder='Username/Email/Phone Number'/>
+            </div>
+            <div className='email'>
+                {/* <label className='label'>National ID</label> */}
+                <input className='input-field' type='password' placeholder='Password'/> 
+            </div>
+            <div>
+                <button className='submit'>Login</button>
+            </div><br/><br/>
+            <p className='paragraph'>Don't have account? <Link to='/signup'>Signup</Link></p><br/>
+            <p className='paragraph'>Forget Password? <Link to='/forget'>Reset</Link></p>
+        </form>
+          </div>
+        </div>
+    </div>
   )
 }
 
